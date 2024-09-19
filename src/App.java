261bbe79cbf77ec7531cbda0eb9bd09cd39688b3
Tomas <no>
2024-09-19 11:21:14 -0300

    Final version

--- src/App.java
+++ src/App.java
@@ -39,6 +39,14 @@ public class App {
           
     public void doMadlib() {
         Scanner scan = new Scanner(System.in);
+        String red = "\u001B[38;2;240;30;10;48;2;255;255;255m";
+        String green = "\u001B[38;2;40;230;80;48;2;220;25;230m";
+        String purple = "\u001B[38;2;160;32;240;48;2;255;160;0m";
+        String cyan = "\u001B[38;2;0;255;255;48;2;1;121;111m";
+        String yellow = "\u001B[38;2;223;211;30;48;2;20;35;245m";
+        String reset = "\u001B[0m";
+        String blue = "\u001B[38;2;22;75;205;48;2;180;255;30m"; 
+
         System.out.println("Enter a Noun ");
         String noun1 = scan.nextLine();
         System.out.println("Enter a verb (past tense) ");
@@ -61,7 +69,7 @@ public class App {
         String person1 = scan.nextLine();
         System.out.println("Enter a physical thing (plural)");
         String thing1 = scan.nextLine();
-        System.out.println("Today you went to buy a " + noun1 + ". After you bought it you "  + verb1 + " away very " + adverb1 + ". You then went home so you could indulge in " + noun2 +  " but you instead craved " + noun3 + ". Out of nowhere a " + disaster1 + " killed your " + pet1 + ". To grieve the pain you went to a " + place1 + " in your Batmobile. However, you wrongly went to a " + place2 + ". Suprisingly you found " + person1 + " while you were there. They gave you a billion dollars and with this you bought a billion " + thing1 + ".");
+        System.out.println("Today you went to buy a " + red + noun1 + reset +  ". After you bought it you " + blue + verb1 + reset + " away very " + green + adverb1 + reset + ". You then went home so you could indulge in " + blue + noun2 + reset +  " but you instead craved " + blue + noun3 + reset + ". Out of nowhere a " + red + disaster1 + reset + " killed your "+ cyan + pet1 + reset +". To grieve the pain you went to a " + purple + place1 + reset + " in your Batmobile. However, you wrongly went to a " + purple + place2 + reset + ". Suprisingly you found "+ cyan + person1 + reset +" while you were there. They gave you a billion dollars and with this you bought a billion " + yellow + thing1 + reset + ".");
 
     }
 
